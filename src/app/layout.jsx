a688a59da5eeace9242e6dcedb0ac4c632f9934b
Prince Gupta <assistus@gmail.com>
2025-12-18 08:04:53 +0530

    title updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "lunchmoney",
+  title: "LunchMoney",
   description: "Lunch Money Application",
   icons: {
     icon: "/favicon.png",
